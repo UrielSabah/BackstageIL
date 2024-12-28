0aa00628f772b2409f30c272e9b15aabec42434d
UrielSabah <urielsabahsoundeng@gmail.com>
2024-12-28 21:23:31 +0200

    Add Grouping for swagger

--- app/main.py
+++ app/main.py
@@ -4,7 +4,11 @@ from app.neondb import insert_music_hall, get_music_hall, update_music_hall
 from app.config import SECRET_KEY
 
 # Define the FastAPI app
-app = FastAPI()
+app = FastAPI(
+    title="MusicHalls API",
+    description="API for technical information for backstage",
+    version="1.0.0"
+)
 
 
 def verify_api_key(api_key: str = Header(...)):
@@ -13,18 +17,18 @@ def verify_api_key(api_key: str = Header(...)):
         raise HTTPException(status_code=403, detail="Invalid API key")
 
 
-@app.get("/")
+@app.get("/", tags=["App"])
 def read_root():
     return {"message": "Hello, World!"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}", tags=["App"])
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
 
 
 # Retrieve a music hall by ID
-@app.get("/music-halls/{hall_id}", response_model=MusicHall)
+@app.get("/music-halls/{hall_id}", response_model=MusicHall, tags=["Music Hall Management"])
 async def fetch_music_hall(hall_id: int):
     try:
         # Call the helper function to get the hall
@@ -35,7 +39,7 @@ async def fetch_music_hall(hall_id: int):
 
 
 # Endpoint to create a new music hall
-@app.post("/music-halls/", response_model=MusicHall)
+@app.post("/music-halls/", response_model=MusicHall, tags=["Music Hall Management"])
 async def create_music_hall(hall: MusicHall, api_key: str = Depends(verify_api_key)):
     # Prepare data tuple for database insertion
     hall_data = (
@@ -61,7 +65,7 @@ async def create_music_hall(hall: MusicHall, api_key: str = Depends(verify_api_k
         raise e
 
 
-@app.put("/music-halls/{hall_id}")
+@app.put("/music-halls/{hall_id}", tags=["Music Hall Management"])
 async def update_hall(hall_id: int, hall_data: UpdateMusicHall, api_key: str = Depends(verify_api_key)):
     # Filter out None values from the input
     try:
