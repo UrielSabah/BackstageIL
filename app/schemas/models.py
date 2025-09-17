f06ef3e1f99b7175c4ee0e313f0a9379035cb9ef
UrielSabah <urielsabahsoundeng@gmail.com>
2025-09-17 21:49:28 +0300

    requierments updated + pydantic models

--- app/schemas/models.py
+++ app/schemas/models.py
@@ -1,27 +1,41 @@
-from pydantic import BaseModel
+from pydantic import BaseModel , Field, EmailStr
+from enum import Enum
 from typing import Optional
 
+class StageType(str, Enum):
+    open = "open"
+    closed = "closed"
+    portable = "portable"
+    raised = "raised"
 
-# Define the request body model for the music hall data
 class MusicHall(BaseModel):
-    city: str
-    hall_name: str
-    email: str
-    stage: bool
-    pipe_height: int
-    stage_type: str
+    city: str = Field(..., min_length=2, max_length=100, description="City where the hall is located")
+    hall_name: str = Field(..., min_length=2, max_length=100, description="Name of the music hall")
+    email: EmailStr = Field(..., description="Contact email")
+    stage: bool = Field(..., description="Whether the hall has a stage")
+    pipe_height: int = Field(..., ge=0, le=100, description="Height of the stage pipes in meters")
+    stage_type: StageType = Field(...,  description="Type of stage")
 
     class Config:
-        from_attributes = True  # To make Pydantic work well with ORM models
-
+        from_attributes = True
+        json_schema_extra = {
+            "example": {
+                "city": "Tel Aviv",
+                "hall_name": "Barby",
+                "email": "info@barby.com",
+                "stage": True,
+                "pipe_height": 30,
+                "stage_type": "raised"
+            }
+        }
 
 class UpdateMusicHall(BaseModel):
-    city: Optional[str] = None
-    hall_name: Optional[str] = None
-    email: Optional[str] = None
+    city: Optional[str] = Field(None, min_length=2, max_length=100)
+    hall_name: Optional[str] = Field(None, min_length=2, max_length=100)
+    email: Optional[EmailStr] = None
     stage: Optional[bool] = None
-    pipe_height: Optional[int] = None
-    stage_type: Optional[str] = None
+    pipe_height: Optional[int] = Field(None, ge=0, le=100)
+    stage_type: Optional[StageType] = None
 
     class Config:
-        from_attributes = True  # To make Pydantic work well with ORM models
+        from_attributes = True
